608372cc8d4ab81467d82b26c207480cd2e7cc79
nkrishnakumar07 <nkrishnakumar22112001@gmail.com>
2024-12-04 18:36:35 +0530

    Navbar Component Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "App.css";
-
+import Navbar from "./components/Navbar";
+import "./App.css";
 const App = () => {
   return (
     <div className="navbar">
       <h1>Logo</h1>
+      <p>Ecommerce website</p>
     </div>
   );
 };
